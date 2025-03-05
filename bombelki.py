6b9239f2a75d1875decf8ff730712560686ebff9
Kaja <130866979+gor-kaya@users.noreply.github.com>
2025-03-05 18:13:40 +0000

    Added a function for creation of bombelki (bubbles) <3

---
+++ bombelki.py
@@ -0,0 +1,8 @@
+def bombelki(n):
+    """
+    This functions takes an integer and prints that many bubbles as you specified.
+    No returns
+    """
+    if type(n) == int:
+        bombelki = n * "o"
+        print(bombelki)
